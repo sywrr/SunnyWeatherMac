0e311146c246224ec2c681d3c14b1ad9ddc47a9f
suny <wrr15212776887@163.com>
2022-05-01 10:29:59 +0800

    second commit.

---
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -0,0 +1,17 @@
+package com.example.sunnyweather
+
+import android.app.Application
+import android.content.Context
+
+class SunnyWeatherApplication :Application() {
+
+    companion object{
+        const val TOKEN ="jGViZoPGdLOOsxgI"
+        lateinit var context: Context
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        context = applicationContext
+    }
+}
